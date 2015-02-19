7ec45df4edd43075a9b8c2f6056610b44aaee827
Stefan Kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2015-02-19 14:26:26 +0100

    Add working ikfast call via database

--- run_ikfast.sh
+++ run_ikfast.sh
@@ -9,8 +9,11 @@ rosrun moveit_ikfast round_collada_numbers.py arm_5dof.dae arm_5dof_rounded.dae 
 # Output info
 openrave-robot.py arm_5dof_rounded.dae --info links
 
-# Try generating translationdirection5d IK
+# Try generating translationdirection5d IK, does not work
 python `openrave-config --python-dir`/openravepy/_openravepy_/ikfast.py --robot=arm_5dof_rounded.dae --iktype=translationdirection5d  --baselink=0 --eelink=5 --savefile=arm_5dof.cpp
+
+# This works for some reason?
+openrave.py --database inversekinematics --robot=arm_5dof.xml --usecached --perftiming=1000 --iktype=translationdirection5d
 
 
 #python `openrave-config --python-dir`/openravepy/_openravepy_/ikfast.py --robot=arm_5dof_rounded.dae --iktype=transform6d  --baselink=0 --eelink=10 --savefile=arm_5dof.cpp
